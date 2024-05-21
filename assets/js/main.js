e56ecb75d69d1da4ba59554ad6e34105d4078392
Mahabub Alam Bhuiyan <ma.nahid960@gmail.com>
2024-05-22 05:38:15 +0600

    done customer till area,and add swiper js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,67 @@ mobileBtn.addEventListener('click', (e) => {
     mobileMenu.classList.toggle("hidden")
 })
 
+
+// swiper slider
+// const swiper = new Swiper('.mySwiper', {
+//     // Optional parameters
+//     slidesPerView: 3.2,
+//     spaceBetween: 30,
+//     loop: true,
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//   });
+
+const swiper = new Swiper('.mySwiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        // when window width is >= 640px
+    640: {
+        slidesPerView: 2,
+      },
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 3,
+      },
+      // when window width is >= 1024px
+      1024: {
+        slidesPerView: 3.2,
+      },
+    },
+    on: {
+      slideChange: function () {
+        const slides = document.querySelectorAll('.swiper-slide');
+        slides.forEach((slide, index) => {
+          if (index >= this.activeIndex && index < this.activeIndex + 3) {
+            slide.style.opacity = '1';
+          } else if (index === this.activeIndex + 3) {
+            slide.style.opacity = '0.5';
+          } else {
+            slide.style.opacity = '1';
+          }
+        });
+      },
+      init: function () {
+        const slides = document.querySelectorAll('.swiper-slide');
+        slides.forEach((slide, index) => {
+          if (index < 3) {
+            slide.style.opacity = '1';
+          } else if (index === 3) {
+            slide.style.opacity = '0.5';
+          } else {
+            slide.style.opacity = '1';
+          }
+        });
+      },
+    },
+  });
